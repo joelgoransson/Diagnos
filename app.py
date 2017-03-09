c7980800b114f5398e299564c37b4683bdcd9ba7
Joel Göransson <kallamackor@gmail.com>
2017-03-09 14:33:41 +0100

    add contact forms

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask("Diagnos")
 def home():
 	return render_template("index.html")
 
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
 if __name__ == "__main__":
 	app.run("0.0.0.0", debug=True)
